f5755f346232b0345251a463efa5fc3877147ceb
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:18:04 +0900

    Deprecated

---
+++ src/DeprecatedTest.java
@@ -0,0 +1,13 @@
+public class DeprecatedTest {
+  public static void main(String[] args) {
+    new WithDeprecated().testMethod();
+  }  
+}
+
+class WithDeprecated {
+  // MEMO: deprecated annotationをつけたメソッドを実行するとwarningが発生する
+  @Deprecated
+  public void testMethod() {
+    System.out.println("this is deprecated method");
+  }
+}
