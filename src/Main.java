c4c66aac4dd0fdea2e5827778e6915be542482e6
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:17:38 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    // Hello Javaを表示する
+    System.out.println("Hello Java");
+
+    System.out.println("args:");
+    // 引数の値を取得する
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(" - " + args[i]);
+    }
+  }
+}
