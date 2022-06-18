1feabce7e8f0c83ca64c8c56e8587a5d5a58db35
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:18:13 +0900

    Javadoc

---
+++ src/JavadocTest.java
@@ -0,0 +1,20 @@
+/**
+ * @author tetsu
+ * @version 0.0.1
+ */
+
+/**
+ * Javadoc試す用のメインクラス
+ */
+
+public class JavadocTest {
+  /**
+   * mainメソッド
+   * 
+   * @param args 使用しない
+   */
+  public static void main(String[] args) {
+    // ディスプレイに表示するためのメソッド
+    System.out.println("Hello Java!");
+  }
+}
