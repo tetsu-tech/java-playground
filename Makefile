9985462f6ecb5f73225aa94dc685ddab5df67a37
tetsu5555 <tetete1118@gmail.com>
2022-06-18 14:05:44 +0900

    Log4JTest

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ compile:
 package:
 	mvn package
 
+# 一旦、-Dexec.mainClassを書き換えて実行するようにする
 start:
-	java -cp target/java-playground-1.0-SNAPSHOT.jar io.tetsutech.ArrayListTest
+	mvn compile exec:java -Dexec.mainClass=io.tetsutech.Log4JTest -Dexec.cleanupDaemonThreads=true
---
+++ src/main/java/io/tetsutech/Log4JTest.java
@@ -0,0 +1,18 @@
+package io.tetsutech;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4JTest {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello, user!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+    }
+}
