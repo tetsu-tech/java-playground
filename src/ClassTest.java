3fc07042d8f648317029dcd6d6a4aa30237eb143
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:36:17 +0900

    class

---
+++ src/ClassTest.java
@@ -0,0 +1,35 @@
+public class ClassTest {
+  public static void main(String[] args) {
+    Counter counter = new Counter(0);
+    counter.increment();
+    System.out.println(counter.count);
+    counter.decrement();
+    System.out.println(counter.count);
+
+    System.out.println(counter.toString());
+  }
+}
+
+class Counter {
+  public int count;
+
+  public Counter(int count) {
+    this.count = count;
+  }
+
+  public void increment() {
+    this.count++;
+  }
+
+  public void decrement() {
+    this.count--;
+  }
+
+  @Override
+  public String toString() {
+    return "Conter{"
+        + "count="
+        + count
+        + '}';
+  }
+}
