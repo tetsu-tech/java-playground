2291570e9d61676eb51e8c44a0bc3c2fddfca78c
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:18:23 +0900

    Override

---
+++ src/OverrideTest.java
@@ -0,0 +1,19 @@
+public class OverrideTest extends TestOverride {
+  // MEMO: Override annotationをつけることでメソッドがOverrideされることを保証する
+  // @Override
+  public void outputs() {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    TestOverride testOverride = new TestOverride();
+    testOverride.outputs();
+  }
+}
+
+class TestOverride {
+  // Overrideしてこっちが実行される
+  public void outputs() {
+    System.out.println("Hello World Override!");
+  }
+}
