997163d6dab8b781fc7da4c4bb9efd6c5c8a4290
tetsu5555 <tetete1118@gmail.com>
2022-06-18 11:17:46 +0900

    ArrayList

---
+++ src/ArrayListTest.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListTest {
+
+  public static void main(String[] args) {
+    declareByList();
+    declareByArrayList();
+  }
+
+  public static void declareByList() {
+    // 宣言の型にListを使う場合
+    List<Integer> arrayList = new ArrayList<Integer>();
+    arrayList.add(10);
+    arrayList.add(20);
+    arrayList.add(30);
+
+    // ArrayListからLinkedListへの変換
+    LinkedList<Integer> linkedList = new LinkedList<Integer>(arrayList);
+    LinkedList<Integer> linkedListCloned = (LinkedList<Integer>) linkedList.clone();
+    for (Integer item : linkedListCloned) {
+      System.out.println(item);
+    }
+  }
+
+  public static void declareByArrayList() {
+    // 宣言の型にArrayListを使う場合
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    arrayList.add(10);
+    arrayList.add(20);
+    arrayList.add(30);
+
+    // ArrayListのコピー
+    //cloneはListインターフェースには存在せず、ArrayListにしかないのでこれを使いたい場合は宣言はArrayListで行いっておいたほうがよいらしい
+    ArrayList<Integer> bar = (ArrayList<Integer>) arrayList.clone();
+    for (Integer i : bar) {
+      System.out.println(i);
+    }
+  }
+}
